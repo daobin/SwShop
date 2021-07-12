4057228bc274a16bfeca06b77d7b309c79de7916
daobin <shouwenlai@foxmail.com>
2021-07-12 17:18:42 +0800

    Commit Test

--- config/oss.php
+++ config/oss.php
@@ -2,8 +2,8 @@
 declare(strict_types=1);
 
 return [
-    'access_key_id' => 'LTAI5tJx2vJZMifCgBgD2Brg',
-    'access_key_secret' => '4kxBSayTlSOSwaAGzd62AbNzc1Kl7J',
-    'endpoint' => 'http://oss-cn-hongkong.aliyuncs.com',
-    'bucket' => 'sw-shop',
+    'access_key_id' => 'xxxxxx',
+    'access_key_secret' => 'xxxxxx',
+    'endpoint' => 'xxxxxx',
+    'bucket' => 'xxxxxx',
 ];
