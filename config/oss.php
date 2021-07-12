046e5aecb35f9e9bae8d70136e902aea3d187d3e
daobin <shouwenlai@foxmail.com>
2021-07-12 17:11:45 +0800

    配置信息

--- config/oss.php
+++ config/oss.php
@@ -2,8 +2,8 @@
 declare(strict_types=1);
 
 return [
-    'access_key_id' => 'LTAI5tJx2vJZMifCgBgD2Brg',
-    'access_key_secret' => '4kxBSayTlSOSwaAGzd62AbNzc1Kl7J',
-    'endpoint' => 'http://oss-cn-hongkong.aliyuncs.com',
-    'bucket' => 'sw-shop',
+    'access_key_id' => 'xxxxx',
+    'access_key_secret' => 'xxxxx',
+    'endpoint' => 'xxxxx',
+    'bucket' => 'xxxxx',
 ];
